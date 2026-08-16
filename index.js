493a3660591ad643440a026b940a1f97fedfddbb
鈴木伸佳 <kobayashielel@gmail.com>
2026-08-17 08:38:25 +0900

    3つのプロミスからの返り値の積を標準出力する方法を２つ書いた

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ function getC() {
   });
 }
 
-// TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
-// ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+getA().then(a =>{
+  getB().then(b =>{
+    getC().then(c =>{
+      console.log(a * b * c);
+    });
+  });
+});
+
+Promise.all([getA(), getB(), getC()]).then(results => {
+  console.log(results[0] * results[1] * results[2]);
+});
